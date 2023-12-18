619554e6b4913269f1cf53d363dfa6322cc15e36
Rostyslav Mykhal <millersdev.ios@gmail.com>
2023-12-18 23:00:19 +0200

    Store
    Stored data

--- ExpenseTracker/ExpenseTracker/Extensions.swift
+++ ExpenseTracker/ExpenseTracker/Extensions.swift
@@ -11,3 +11,20 @@ import SwiftUI
 extension Color {
     static let systemBackground = Color(.systemBackground)
 }
+
+extension DateFormatter {
+    static let allNumericUSA: DateFormatter = {
+        print("Initializing DateFormatter")
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd/yyyy"
+        
+        return formatter
+    }()
+}
+
+extension String {
+    func dateParsed() -> Date {
+        guard let parsedDate = DateFormatter.allNumericUSA.date(from: self) else {return Date()}
+        return parsedDate
+    }
+}
--- ExpenseTracker/ExpenseTracker/TransactionModel.swift
+++ ExpenseTracker/ExpenseTracker/TransactionModel.swift
@@ -7,23 +7,31 @@
 
 import Foundation
 
-struct Transaction: Identifiable {
+struct Transaction: Identifiable, Decodable {
     let id: Int
     let date: String
     let institution: String
     let account: String
     var merchant: String
     let amount: Double
-    let type = TransactionType.RawValue
+    let type: TransactionType.RawValue
     var categoryId: Int
     var category: String
     let isPending: Bool
     var isTransfer: Bool
-    var isExpence: Bool
+    var isExpense: Bool
     var isEdited: Bool
+    
+    var dateParsed: Date {
+        date.dateParsed()
+    }
+    
+    var signedAmount: Double {
+        return type == TransactionType.credit.rawValue ? amount: -amount
+    }
 }
 
-enum transactionType: String {
-    case dabit = "debit"
+enum TransactionType: String {
+    case debit = "debit"
     case credit = "credit"
 }
